779dfb8c4b8617aad4ab47254e09928f66101782
Capobwoy <che3tolol03@gmail.com>
2025-10-29 09:46:30 +0100

    29/10/25

---
+++ workspace/CodificacionFormularios/src/main/java/com/example/demo/PruebaController.java
@@ -0,0 +1,35 @@
+package com.example.demo;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class PruebaController {
+	
+	@GetMapping("/")
+	public String mostrarFormulario() {
+		return "formulario";
+	}
+	
+	@GetMapping("/procesarGet")
+	public String procesarGet(@RequestParam String texto, Model model) {
+		model.addAttribute("metodo", "GET");
+		model.addAttribute("textoRecibido", texto);
+		model.addAttribute("textoCodificado", URLEncoder.encode(texto, StandardCharsets.UTF_8));
+		return "resultado";
+	}
+	
+	@PostMapping("/procesarPost")
+	public String procesarPost(@RequestParam String texto, Model model) {
+		model.addAttribute("metodo", "POST");
+		model.addAttribute("textoRecibido", texto);
+		return "resultado";
+	}
+
+}
